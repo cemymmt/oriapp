664ea99c4a7db6ddf842fec0c3e664682d6eb4b5
Chiemi Yamamoto <chrn14694@etude.ocn.ne.jp>
2017-09-05 18:39:24 +0900

    unilists を uniに名前変更 uni/:id ページより大学専用ページのトップ画面を作成

--- test/controllers/unis_controller_test.rb
+++ test/controllers/unis_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UnilistsControllerTest < ActionDispatch::IntegrationTest
+class UnisControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end

a9758708638157ba24ce434a097728d50d299660
Chiemi Yamamoto <chrn14694@etude.ocn.ne.jp>
2017-09-25 16:41:24 +0900

    responsive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922055110) do
+ActiveRecord::Schema.define(version: 20170925070550) do
 
   create_table "bookings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.text     "text",         limit: 65535
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20170922055110) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.integer  "uni_id"
     t.index ["email"], name: "index_students_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true, using: :btree
   end

8d88c3234d33b04cb597d03218579b7dec036971
Chiemi Yamamoto <chrn14694@etude.ocn.ne.jp>
2017-09-28 13:57:41 +0900

    view

--- app/models/uni.rb
+++ app/models/uni.rb
@@ -2,6 +2,6 @@ class Uni < ApplicationRecord
   has_many :discounts
   has_many :eventdates
   has_many :eventdetails
-
+  has_many :students
   scope :incremental_search, -> (name){ where('name LIKE(?)',"%#{name}%") }
 end
